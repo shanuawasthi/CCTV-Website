1c408a75fabc12831291fb933669538594510a1e
shanuawasthi <shanuawasthi531@gmail.com>
2026-02-23 10:25:36 +0530

    Updated backend models, HTML, JS and products page

--- backend/models/service.js
+++ backend/models/service.js
@@ -1,11 +1,4 @@
-// const mongoose = require("mongoose");
 
-// const serviceSchema = new mongoose.Schema({
-//    title: String,
-//    description: String
-// });
-
-// module.exports = mongoose.model("Service", serviceSchema);
 const mongoose = require('mongoose');
 
 const serviceSchema = new mongoose.Schema({
